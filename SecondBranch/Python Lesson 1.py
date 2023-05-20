df790beac7c99f2214de736d954bef6f0bea0629
janiceb75 <janicebailey75@gmail.com>
2023-05-20 19:23:00 -0400

    add a python file

---
+++ SecondBranch/Python Lesson 1.py
@@ -0,0 +1,61 @@
+college= "Technical Collge"
+          # 01234567
+# print(college[:9])
+# print(college[10:])
+first_name=college[:9] #slicing
+
+
+
+# print (college.replace("Technical","Electrical"))
+# print (college.find("C"))
+# print (college.capitalize())
+# college = college.lower()
+# print (college.islower())
+# my_num = 5
+# print (str(my_num) + " my favorite")
+# my_num = -5
+# print(abs(my_num))
+# my_num = -5
+# print(pow(3,2))
+# print(max(4, 6))
+# print(min(4,6))
+# print(round(3.7))
+
+from math import *
+# print(floor(3.7))
+# print(ceil(3.7))
+# print(sqrt(64))
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "!" + age + " is a good age")
+# num1 = input ("Enter a number: ")
+# num2 = input ("Enter another number: ")
+# result = float(num1) + float(num2)
+# print (result)
+# Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string.
+# Go to the editorSample String : 'abc', 'xyz'Expected Result : 'xyc abz'
+string1 = "jumping"
+# string2 = " fish"
+# p_index=string1.find("p")
+# slic1=string1[:p_index]
+# print(slic1)
+# slice1 = string1[:2]
+# slice2 = string2[:3]
+# slice3 = string1[2:]
+# slice4 = string2[3:]
+# newstring = slice1 + slice4 #contatination operator
+# newstring2 = slice2 + slice3
+#
+# print (newstring)
+# print (newstring2)
+
+#
+# string1="Arbaaz"
+# string2="ali"
+# string3=string1+" "+string2
+# print(string3)
+# first_name = input ("What is your first name: ")
+# last_name = input ("What is your last name: ")
+# full_name = first_name +" " + last_name
+# print (full_name.upper())
+
